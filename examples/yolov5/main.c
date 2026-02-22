f33e65ca27ae3e5a2a30b3d6f1d9fb32e6d6b57f
Chris Nakovski <chrisnakovski@gmail.com>
2026-02-22 05:32:15 -0800

    yolov5: run webcam inference in live loop

--- examples/yolov5/main.c
+++ examples/yolov5/main.c
@@ -1,55 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 #include <awnn_lib.h>
 
-#include "image_utils.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+
 #include "yolov5_pre_process.h"
 #include "yolov5_post_process.h"
 
 int main(int argc, char **argv) {
-    printf("%s nbg input\n", argv[0]);
-    if(argc < 3)
+    printf("%s nbg [camera_index] [frame_path]\n", argv[0]);
+    if (argc < 2)
     {
         printf("Arguments count %d is incorrect!\n", argc);
         return -1;
     }
+
     const char* nbg = argv[1];
-    const char* input = argv[2];
+    int camera_index = (argc >= 3) ? atoi(argv[2]) : 0;
+    const char* frame_path = (argc >= 4) ? argv[3] : "/tmp/yolov5_webcam_frame.jpg";
+
+    cv::VideoCapture cap(camera_index);
+    if (!cap.isOpened()) {
+        fprintf(stderr, "Failed to open webcam index %d\n", camera_index);
+        return -1;
+    }
 
     // npu init
     awnn_init();
     // create network
     Awnn_Context_t *context = awnn_create(nbg);
-    // copy input
-    unsigned int input_width = 640;
-    unsigned int input_height = 640;
-    unsigned int input_depth = 3;
 
-    unsigned int sz = input_width * input_height * input_depth;
-    // unsigned char* plant_data = (unsigned char*) malloc(sz * sizeof(unsigned char));
-    unsigned char* plant_data;
-    unsigned int i, j;
-    unsigned int file_size;
+    cv::namedWindow("yolov5_webcam", cv::WINDOW_AUTOSIZE);
 
-    // preprocess
-    plant_data = yolov5_pre_process(input, &file_size);
+    while (1) {
+        cv::Mat frame;
+        cap >> frame;
+        if (frame.empty()) {
+            fprintf(stderr, "Failed to capture frame from webcam.\n");
+            break;
+        }
 
-    void *input_buffers[] = {plant_data};
-    awnn_set_input_buffers(context, input_buffers);
-    // process network
-    awnn_run(context);
-    // get result
-    float **results = awnn_get_output_buffers(context);
-    // post process
-    yolov5_post_process(input, results);
+        if (!cv::imwrite(frame_path, frame)) {
+            fprintf(stderr, "Failed to save frame to %s\n", frame_path);
+            break;
+        }
 
-    free(plant_data);
-    // destroy network
+        unsigned int file_size = 0;
+        unsigned char* plant_data = yolov5_pre_process(frame_path, &file_size);
+        if (!plant_data) {
+            fprintf(stderr, "Pre-process failed.\n");
+            break;
+        }
+
+        void *input_buffers[] = {plant_data};
+        awnn_set_input_buffers(context, input_buffers);
+        awnn_run(context);
+
+        float **results = awnn_get_output_buffers(context);
+        yolov5_post_process(frame_path, results);
+
+        free(plant_data);
+
+        cv::Mat vis = cv::imread("result.png", 1);
+        if (vis.empty()) {
+            fprintf(stderr, "Failed to load result.png for display\n");
+            break;
+        }
+
+        cv::imshow("yolov5_webcam", vis);
+        int key = cv::waitKey(1);
+        if (key == 'q' || key == 'Q' || key == 27) {
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
+    cap.release();
+
     awnn_destroy(context);
-    // npu uninit
     awnn_uninit();
 
     return 0;
